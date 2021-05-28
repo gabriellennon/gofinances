6bd5b23d3ba595960799e94912cb2b24f57c857d
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-05-28 19:09:57 -0300

    new component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,12 +10,57 @@ import {
     UserName,
     UserWrapper,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionsList
 } from './styles';
 
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransationCardProps } from '../../components/TransactionCard';
+
+export interface DataListProps extends TransationCardProps {
+    id: string;
+}
 
 export function Dashboard(){
+
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Hamburgueria Pizzy",
+            amount: "R$ 59,00",
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: "17/04/2020"
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: "Aluguel do apartamento",
+            amount: "R$ 1.200,00",
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: "23/04/2020"
+        },
+    ];
+
     return(
         <Container>
             <Header>
@@ -38,10 +83,35 @@ export function Dashboard(){
                 // showsHorizontalScrollIndicator={false}
                 // contentContainerStyle={{ paddingHorizontal: 24 }}
             >
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard 
+                    type="up"
+                    title="Entradas" 
+                    amount="R$ 17.400,00" 
+                    lastTransaction="Última entrada dia 13 de abril" 
+                />
+                <HighlightCard 
+                    type="down"
+                    title="Saídas" 
+                    amount="R$ 1.259,00" 
+                    lastTransaction="Última saída dia 03 de abril" 
+                />
+                <HighlightCard
+                    type="total" 
+                    title="Total" 
+                    amount="R$ 16.141,00" 
+                    lastTransaction="01 à 16 de abril" 
+                />
+                
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionsList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-
+import { FlatList } from 'react-native';
 import { Feather} from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 //coloco em letra maiuscula para que o react entenda que é um component
 export const Container = styled.View`
@@ -17,13 +21,13 @@ export const Header = styled.View`
 
     background-color: ${({ theme }) => theme.colors.primary};
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
     width: 100%;
-
+    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
     padding: 0 24px;
     flex-direction: row;
     justify-content: space-between;
@@ -70,5 +74,34 @@ export const HighlightCards = styled.ScrollView.attrs({
     showsHorizontalScrollIndicator: false,
     contentContainerStyle: {paddingHorizontal: 24}
 })`
-    
+    width: 100%;
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
+
+
+export const Transactions = styled.View`
+    flex: 1;
+    padding: 0 24px;
+    margin-top: ${RFPercentage(12)}px;
+
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+    margin-bottom: 16px;
+`;
+
+export const TransactionsList = styled(
+    //falando que a flatlist é um novo e tem um tipagem
+    FlatList as new () => FlatList<DataListProps>
+    ).attrs({
+    showsVerticalScrollIndicator: false,
+    //o card nao vai ficar por cima do detalhe de minimizar do iphone que tem a partir do X
+    contentContainerStyle: {
+        paddingBottom: getBottomSpace()
+    }
+})`
+
 `;
