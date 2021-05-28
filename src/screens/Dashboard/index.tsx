eb405c2f235ba88a37a0e5714f63a835466ddf97
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-05-27 21:28:18 -0300

    new screen and component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-
-import {
-    Text,
-
-} from 'react-native';
+import {} from 'react-native';
 import { 
     Container, 
-    Title 
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    Icon,
+    HighlightCards
 } from './styles';
+
+import { HighlightCard } from '../../components/HighlightCard';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri:  'https://avatars.githubusercontent.com/u/57332512?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Gabriel</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards
+                // ddeixar eles lado a lado
+                // horizontal
+                // para ocultar a barrinha horizontal que aparece
+                // showsHorizontalScrollIndicator={false}
+                // contentContainerStyle={{ paddingHorizontal: 24 }}
+            >
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
         </Container>
     )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,17 +1,74 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+import { Feather} from '@expo/vector-icons';
 
 //coloco em letra maiuscula para que o react entenda que é um component
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-
     background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-    font-family: ${({ theme }) => theme.fonts.bold};
-    font-size: 24px;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+    width: 100%;
+    /* estamos trabalhando com proporcao, caso entre em outro app ele se ajusta */
+    /* usando o pixel mas ja convertido na proporcao */
+    height: ${RFPercentage(42)}px;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const UserWrapper = styled.View`
+    width: 100%;
+
+    padding: 0 24px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const UserInfo = styled.View`
+    /* aplicando o flex no texto do titulo */
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const  Photo = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.shape };
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+`;
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.shape };
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.bold };
+`;
+
+//estilizando e ja importando o icone aqui
+export const Icon = styled(Feather)`
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
+
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: {paddingHorizontal: 24}
+})`
+    
 `;
